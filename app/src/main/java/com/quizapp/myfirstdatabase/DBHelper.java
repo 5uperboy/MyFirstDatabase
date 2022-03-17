283a7e0eed69b0074f5b1b4bce45bfbdab917c5a
Johnrod <johnrodjardeleza09@gmail.com>
2022-03-17 22:29:01 +0800

    Main Project for MyFirstDatabase

--- app/src/main/java/com/quizapp/myfirstdatabase/DBHelper.java
+++ app/src/main/java/com/quizapp/myfirstdatabase/DBHelper.java
@@ -7,35 +7,36 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class DBHelper extends SQLiteOpenHelper {
+            public class DBHelper extends SQLiteOpenHelper {
 
-    public DBHelper(Context context) {
-        super(context, "Userdata.db", null, 1);
-    }
+                public DBHelper(Context context) {
+                    super(context, "Userdata.db", null, 1);
+                }
 
-    @Override
-    public void onCreate(SQLiteDatabase DB) {
-        DB.execSQL("create Table Userdetails(name Text primary key, contact TEXT)");
-    }
+                @Override
+                public void onCreate(SQLiteDatabase DB) {
+                    DB.execSQL("create Table Userdetails(name Text primary key, contact TEXT)");
+                }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
-        DB.execSQL("drop Table if exists Userdetails");
-    }
+                @Override
+                public void onUpgrade(SQLiteDatabase DB, int oldVersion, int newVersion) {
+                    DB.execSQL("drop Table if exists Userdetails");
+                }
 
-    public boolean insertuserdata(String name, String contact){
-        SQLiteDatabase DB = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("contact", contact);
+                public boolean insertuserdata(String name, String contact){
+                    SQLiteDatabase DB = this.getWritableDatabase();
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put("name", name);
+                    contentValues.put("contact", contact);
 
-        long result = DB.insert("Userdetails", null, contentValues);
 
-        if (result == -1){
-            return false;
-        }
-        else{
-            return true;
+                    long result = DB.insert("Userdetails", null, contentValues);
+
+                    if (result == -1){
+                        return false;
+                    }
+                    else{
+                        return true;
         }
     }
 }
